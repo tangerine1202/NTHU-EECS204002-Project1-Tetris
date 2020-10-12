67231235ea046022616c13b8c8e8112153f7c3f5
Alan Huang <alanhuang@sd206026.shuo.ab.nthu.edu.tw>
2020-10-12 21:03:09 +0800

    feat: add debug param

--- src/utils.h
+++ src/utils.h
@@ -1,27 +1,42 @@
 #ifndef UTILS_H
 #define UTILS_H
+// enable debug mode
+#define DEBUG_MODE
 
 #include <iostream>
 #include <string>
 
-namespace utils {
+namespace utils
+{
 
   using std::cout;
   using std::string;
 
-  void debug(string str){
+  void debug(string str)
+  {
+#ifdef DEBUG_MODE
     cout << "[DEBUG] " << str << '\n';
+#endif
   }
-  void info(string str){
+  void info(string str)
+  {
+#ifdef DEBUG_MODE
     cout << "[INFO] " << str << '\n';
+#endif
   }
-  void warning(string str){
+  void warning(string str)
+  {
+#ifdef DEBUG_MODE
     cout << "[WARN] " << str << '\n';
+#endif
   }
-  void error(string str) {
+  void error(string str)
+  {
+#ifdef DEBUG_MODE
     cout << "[ERROR] " << str << '\n';
+#endif
   }
 
-};
+}; // namespace utils
 
 #endif
